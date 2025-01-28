45bbb46a5f03c1a3af5fdf87da6c34d1e7709148
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-01-28 10:14:04 +0530

    Displayed Genres

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,16 @@ const App = () => {
         base: `"nav" "main"`,
         lg: ` "nav nav" "sidebar main"`,
       }}
+      templateColumns={{
+        base : '1fr',
+        lg : "200px 1fr"
+      }}
     >
       <GridItem area={"nav"}>
         <NavBar />
       </GridItem>
 
-      <GridItem hideBelow={"lg"} area={"sidebar"} >
+      <GridItem paddingX={8} hideBelow={"lg"} area={"sidebar"} >
     <GenreList />
       </GridItem>
 
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,29 @@
 // GenreList.tsx file
 
+import { HStack, Image, List, Text } from "@chakra-ui/react";
 import useGenre from "./hooks/useGenre";
+import CropImageUrl from "@/services/URLImageCrop";
 
 const GenreList = () => {
   const { data } = useGenre();
 
   return (
-    <ul>
+    <List.Root>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <List.Item key={genre.id} as="ul">
+          <HStack my={2}>
+            <Image
+              boxSize="42px"
+              borderRadius="22px"
+              src={CropImageUrl(genre.image_background)}
+            ></Image>
+            <Text fontSize="lg" mx={1}>
+              {genre.name}
+            </Text>
+          </HStack>
+        </List.Item>
       ))}
-    </ul>
+    </List.Root>
   );
 };
 
